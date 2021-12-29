4b48bce6ee6b56ac69032f9f8a9ff90c82a8a353
staponkust <staponkust@gmail.com>
2021-12-29 22:13:51 +0200

    Create gallery.js
    baigiamojo galerija

---
+++ js/gallery.js
@@ -0,0 +1,10 @@
+function didintiImg(img) {
+    img.style.transform = "scale(1.5)";
+    img.style.transition = "transform 0.25s ease";
+    img.style.zIndex = 9;
+}
+function grazintiImg(img) {
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.25s ease";
+    img.style.zIndex = 1;
+}
