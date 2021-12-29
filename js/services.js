ea080dd13789f83bff294cfb594e1b1e36d77621
staponkust <staponkust@gmail.com>
2021-12-29 22:15:20 +0200

    Baigiamojo servisai
    Atliekamu paslaugu aprasymas su vanila

---
+++ js/services.js
@@ -0,0 +1,12 @@
+function kviestiRankos() {
+    document.getElementById('manikiuras_list').style.opacity = 100;
+}
+function sleptiRankos() {
+    document.getElementById('manikiuras_list').style.opacity = 0;
+}
+function kviestiPeda() {
+    document.getElementById('pedikiuras_list').style.opacity = 100;
+}
+function sleptiPeda() {
+    document.getElementById('pedikiuras_list').style.opacity = 0;
+}
